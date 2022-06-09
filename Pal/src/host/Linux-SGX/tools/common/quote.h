eb24ef808253e8857a3a33295b56d107f8c3975a
Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>
2022-06-09 07:40:14 -0700

    [Pal/Linux-SGX] tools: Clean up IAS-related functions
    EPID attestation scheme communicates with web-based Intel Attestation
    Service (IAS) to send SGX quotes and receive IAS-signed SGX attestation
    reports. Previously, the functions pertaining to IAS were located in
    unrelated files and had confusing names. This commit fixes this:
    
    - `attestation.[c|h]` renamed to `quote.[c|h]`
    - `ias_verify_quote()` renamed to `ias_send_quote_get_report()`
    - `ias_verify_quote_raw()` renamed to `ias_send_quote_get_report_raw()`
    - `verify_ias_report_extract_quote()` renamed to
      `ias_verify_report_extract_quote()` and moved to `ias.[c|h]`
    
    Signed-off-by: Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>

--- Pal/src/host/Linux-SGX/tools/common/quote.h
+++ Pal/src/host/Linux-SGX/tools/common/quote.h
@@ -3,8 +3,8 @@
  *                    Rafal Wojdyla <omeg@invisiblethingslab.com>
  */
 
-#ifndef ATTESTATION_H
-#define ATTESTATION_H
+#ifndef QUOTE_H
+#define QUOTE_H
 
 #include <stdbool.h>
 #include <stdint.h>
@@ -20,30 +20,6 @@
  *  \param[in] quote_size Size of \a quote_data in bytes.
  */
 void display_quote(const void* quote_data, size_t quote_size);
-
-/*!
- *  \brief Verify IAS attestation report. Also extract the SGX quote contained in IAS report:
- *         allocate enough memory to hold the quote and pass it to the user.
- *
- *  \param[in] ias_report         IAS attestation verification report.
- *  \param[in] ias_report_size    Size of \a ias_report in bytes.
- *  \param[in] ias_sig_b64        IAS report signature (base64-encoded as returned by IAS).
- *  \param[in] ias_sig_b64_size   Size of \a ias_sig_b64 in bytes.
- *  \param[in] allow_outdated_tcb Treat IAS status codes: GROUP_OUT_OF_DATE, CONFIGURATION_NEEDED,
- *                                SW_HARDENING_NEEDED, CONFIGURATION_AND_SW_HARDENING_NEEDED as OK.
- *  \param[in] nonce              (Optional) Nonce that's expected in the report.
- *  \param[in] ias_pub_key_pem    (Optional) IAS public RSA key (PEM format, NULL-terminated).
- *                                If not specified, a hardcoded Intel's key is used.
- *  \param[out] out_quote         Buffer with quote. User is responsible for freeing it.
- *  \param[out] out_quote_size    Size of \a out_quote in bytes.
- *
- *  \return 0 on successful verification, negative value on error.
- */
-int verify_ias_report_extract_quote(const uint8_t* ias_report, size_t ias_report_size,
-                                    uint8_t* ias_sig_b64, size_t ias_sig_b64_size,
-                                    bool allow_outdated_tcb, const char* nonce,
-                                    const char* ias_pub_key_pem, uint8_t** out_quote,
-                                    size_t* out_quote_size);
 
 /*!
  *  \brief Verify that the provided SGX quote body contains expected values.
@@ -77,4 +53,4 @@ int verify_quote_body(const sgx_quote_body_t* quote_body, const char* mr_signer,
  */
 int verify_quote_body_enclave_attributes(sgx_quote_body_t* quote_body, bool allow_debug_enclave);
 
-#endif /* ATTESTATION_H */
+#endif /* QUOTE_H */
